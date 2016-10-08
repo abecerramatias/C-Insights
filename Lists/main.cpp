6a385b19c8749d78dd2d766612f867d72e289bd2
Alberto Becerra Matias <abeceramatias@gmail.com>
2016-10-08 10:42:05 -0400

    Initial Commit

---
+++ Lists/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lists
+//
+//  Created by Alberto Becerra on 10/8/16.
+//  Copyright © 2016 Alberto Becerra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
